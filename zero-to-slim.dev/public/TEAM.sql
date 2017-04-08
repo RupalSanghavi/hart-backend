71dd2cdced79092995623d05468b33f3b8b1c0b2
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-07 21:40:19 -0500

    mock data for class,student,team tables

---
+++ zero-to-slim.dev/public/TEAM.sql
@@ -0,0 +1,2 @@
+insert into TEAM (id, name, logo, blog, charterCompleted) values (2, 'Adams Group', 'http://dummyimage.com/121x123.jpg/5fa2dd/ffffff', 'Praesent blandit.', true);
+insert into TEAM (id, name, logo, blog, charterCompleted) values (1, 'Homenick, Runte and Harvey', 'http://dummyimage.com/144x210.jpg/dddddd/000000', 'Maecenas rhoncus aliquam lacus.', true);
