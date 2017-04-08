27923382140f1d010cf91fdaa399d5eaf189e1f9
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-08 03:17:54 -0500

    updated Class and student tables data

--- zero-to-slim.dev/public/CLASS.sql
+++ zero-to-slim.dev/public/CLASS.sql
@@ -1,5 +1,1 @@
-insert into CLASS (id, year, semester, section) values (1, 2014, 'Spring', 2);
-insert into CLASS (id, year, semester, section) values (2, 2016, 'Spring', 1);
-insert into CLASS (id, year, semester, section) values (3, 2017, 'Fall', 1);
-insert into CLASS (id, year, semester, section) values (4, 2015, 'Fall', 1);
-insert into CLASS (id, year, semester, section) values (5, 2016, 'Fall', 1);
+insert into CLASS (id, year, semester, section) values (1, 2015, 'Fall', 1);
