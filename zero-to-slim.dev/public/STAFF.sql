df4c71900fdc3b815eebe9a004061d91a68ec40e
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-08 08:38:48 -0500

    more mock data

---
+++ zero-to-slim.dev/public/STAFF.sql
@@ -0,0 +1,3 @@
+insert into STAFF (id, admin, last_name, first_name) values (1, 1, 'Johnston', 'Robert');
+insert into STAFF (id, admin, last_name, first_name) values (2, 1, 'Palmer', 'Marilyn');
+insert into STAFF (id, admin, last_name, first_name) values (3, 1, 'Shaw', 'Gloria');
