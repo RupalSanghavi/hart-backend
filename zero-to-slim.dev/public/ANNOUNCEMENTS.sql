181c85ba8b1d4f537f0b36228f3abcbac243684c
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-08 08:26:19 -0500

    more tables mock data:

---
+++ zero-to-slim.dev/public/ANNOUNCEMENTS.sql
@@ -0,0 +1,5 @@
+insert into ANNOUNCEMENTS (title, body, create_date, priority, creator, id, STAFF_id) values ('imperdiet et', 'Quisque ut erat.', '2017-02-12', 1, 'Denise Alexander', 1, 1);
+insert into ANNOUNCEMENTS (title, body, create_date, priority, creator, id, STAFF_id) values ('dolor', 'Pellentesque viverra pede ac diam.', '2016-08-07', 2, 'Howard Mason', 2, 2);
+insert into ANNOUNCEMENTS (title, body, create_date, priority, creator, id, STAFF_id) values ('elementum nullam', 'Nulla mollis molestie lorem.', '2017-03-25', 3, 'Jennifer Reed', 3, 3);
+insert into ANNOUNCEMENTS (title, body, create_date, priority, creator, id, STAFF_id) values ('at', 'Nam tristique tortor eu pede.', '2017-02-02', 4, 'Rose Ramirez', 4, 1);
+insert into ANNOUNCEMENTS (title, body, create_date, priority, creator, id, STAFF_id) values ('curae duis faucibus', 'Maecenas ut massa quis augue luctus tincidunt.', '2016-07-31', 5, 'Beverly Ryan', 5, 2);
