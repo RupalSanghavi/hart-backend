119c5efdfef5b5db6cd02d0520649f49ca947292
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-08 09:21:56 -0500

    rest of the mock data

---
+++ zero-to-slim.dev/public/RESOURCES.sql
@@ -0,0 +1,3 @@
+insert into RESOURCES (id, link, name, category) values (1, 'www.github.com', 'Github', 'Source Control');
+insert into RESOURCES (id, link, name, category) values (2, 'www.asana.com', 'Asana', 'Project Management');
+insert into RESOURCES (id, link, name, category) values (3, 'www.jira.com', 'Jira', 'Project Management');
---
+++ zero-to-slim.dev/public/TEAM_ROLES.sql
@@ -0,0 +1,9 @@
+insert into TEAM_ROLES (id, name) values (1, 'Executive');
+insert into TEAM_ROLES (id, name) values (2, 'Explorer');
+insert into TEAM_ROLES (id, name) values (3, 'Innovator');
+insert into TEAM_ROLES (id, name) values (4, 'Analyst');
+insert into TEAM_ROLES (id, name) values (5, 'Driver');
+insert into TEAM_ROLES (id, name) values (6, 'Chairman');
+insert into TEAM_ROLES (id, name) values (7, 'Completer');
+insert into TEAM_ROLES (id, name) values (8, 'Team player');
+insert into TEAM_ROLES (id, name) values (9, 'Expert');
