85c4d3c8b267409c335e1e14dc6578a24b3082a8
Rupal Sanghavi <rsanghavi@smu.edu>
2017-04-15 22:31:20 -0500

    TOOLS mock data

---
+++ zero-to-slim.dev/public/TOOLS.sql
@@ -0,0 +1,27 @@
+create table TOOLS (
+	id INT,
+	comm_tool VARCHAR(7),
+	STUDENT_id VARCHAR(2)
+);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (1, 'Github', 1);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (2, 'Asana', 2);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (3, 'Jira', 3);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (4, 'Slack', 4);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (5, 'Groupme', 5);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (6, 'Github', 6);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (7, 'Asana', 7);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (8, 'Jira', 8);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (9, 'Slack', 10);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (10, 'Groupme', 1);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (11, 'Github', 2);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (12, 'Asana', 3);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (13, 'Jira', 4);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (14, 'Slack', 5);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (15, 'Groupme', 6);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (16, 'Github', 7);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (17, 'Asana', 8);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (18, 'Jira', 10);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (19, 'Slack', 1);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (20, 'Groupme', 2);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (21, 'Github', 3);
+insert into TOOLS (id, comm_tool, STUDENT_id) values (22, 'Asana', 4);
